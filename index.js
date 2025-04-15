0f06257da33bd29508a1f785fa93da5c01baeca8
omari3-blif <benwhittaker776@gmail.com>
2025-04-15 11:34:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-
-// Ben Whittaker Tech - WhatsApp Bot with 200+ Features and Web Server
-
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const axios = require('axios');
 const fs = require('fs');
-const os = require('os');
-const moment = require('moment');
 const express = require('express');
 require('dotenv').config();
 
@@ -33,34 +28,25 @@ client.on('message', async msg => {
     const message = msg.body.toLowerCase();
     const chat = await msg.getChat();
 
-    // General Commands
+    // Basic Commands
     if (message === 'ping') return msg.reply('pong');
-    if (message === 'time') return msg.reply(`⏰ ${moment().format('LLLL')}`);
+    if (message === 'time') return msg.reply(`⏰ ${new Date().toLocaleString()}`);
     if (message.startsWith('say ')) return msg.reply(msg.body.slice(4));
     if (message === 'menu') {
-        return msg.reply(`*Ben Whittaker Bot Menu*
-Total features: 200+
-Type any command to try:
-- ping
-- time
-- say <text>
-- record
-- type
-- joke
-- feature1 - feature200`);
+        return msg.reply(`*Ben Whittaker Bot Menu*\nTotal features: 200+\nType any command to try:\n- ping\n- time\n- say <text>\n- ai <question>\n- record\n- type\n- joke\n- sticker\n- feature1 - feature200`);
     }
 
+    // Fake Actions
     if (message === 'record') {
         await chat.sendStateRecording();
         return msg.reply('_Recording simulation..._');
     }
-
     if (message === 'type') {
         await chat.sendStateTyping();
         return msg.reply('_Typing simulation..._');
     }
 
-    // Fun Commands
+    // Jokes
     if (message === 'joke') {
         const jokes = [
             "Why don't scientists trust atoms? Because they make up everything!",
@@ -70,208 +56,52 @@ Type any command to try:
         return msg.reply(jokes[Math.floor(Math.random() * jokes.length)]);
     }
 
-    if (message === 'feature1') return msg.reply('Feature 1 triggered!');
-    if (message === 'feature2') return msg.reply('Feature 2 triggered!');
-    if (message === 'feature3') return msg.reply('Feature 3 triggered!');
-    if (message === 'feature4') return msg.reply('Feature 4 triggered!');
-    if (message === 'feature5') return msg.reply('Feature 5 triggered!');
-    if (message === 'feature6') return msg.reply('Feature 6 triggered!');
-    if (message === 'feature7') return msg.reply('Feature 7 triggered!');
-    if (message === 'feature8') return msg.reply('Feature 8 triggered!');
-    if (message === 'feature9') return msg.reply('Feature 9 triggered!');
-    if (message === 'feature10') return msg.reply('Feature 10 triggered!');
-    if (message === 'feature11') return msg.reply('Feature 11 triggered!');
-    if (message === 'feature12') return msg.reply('Feature 12 triggered!');
-    if (message === 'feature13') return msg.reply('Feature 13 triggered!');
-    if (message === 'feature14') return msg.reply('Feature 14 triggered!');
-    if (message === 'feature15') return msg.reply('Feature 15 triggered!');
-    if (message === 'feature16') return msg.reply('Feature 16 triggered!');
-    if (message === 'feature17') return msg.reply('Feature 17 triggered!');
-    if (message === 'feature18') return msg.reply('Feature 18 triggered!');
-    if (message === 'feature19') return msg.reply('Feature 19 triggered!');
-    if (message === 'feature20') return msg.reply('Feature 20 triggered!');
-    if (message === 'feature21') return msg.reply('Feature 21 triggered!');
-    if (message === 'feature22') return msg.reply('Feature 22 triggered!');
-    if (message === 'feature23') return msg.reply('Feature 23 triggered!');
-    if (message === 'feature24') return msg.reply('Feature 24 triggered!');
-    if (message === 'feature25') return msg.reply('Feature 25 triggered!');
-    if (message === 'feature26') return msg.reply('Feature 26 triggered!');
-    if (message === 'feature27') return msg.reply('Feature 27 triggered!');
-    if (message === 'feature28') return msg.reply('Feature 28 triggered!');
-    if (message === 'feature29') return msg.reply('Feature 29 triggered!');
-    if (message === 'feature30') return msg.reply('Feature 30 triggered!');
-    if (message === 'feature31') return msg.reply('Feature 31 triggered!');
-    if (message === 'feature32') return msg.reply('Feature 32 triggered!');
-    if (message === 'feature33') return msg.reply('Feature 33 triggered!');
-    if (message === 'feature34') return msg.reply('Feature 34 triggered!');
-    if (message === 'feature35') return msg.reply('Feature 35 triggered!');
-    if (message === 'feature36') return msg.reply('Feature 36 triggered!');
-    if (message === 'feature37') return msg.reply('Feature 37 triggered!');
-    if (message === 'feature38') return msg.reply('Feature 38 triggered!');
-    if (message === 'feature39') return msg.reply('Feature 39 triggered!');
-    if (message === 'feature40') return msg.reply('Feature 40 triggered!');
-    if (message === 'feature41') return msg.reply('Feature 41 triggered!');
-    if (message === 'feature42') return msg.reply('Feature 42 triggered!');
-    if (message === 'feature43') return msg.reply('Feature 43 triggered!');
-    if (message === 'feature44') return msg.reply('Feature 44 triggered!');
-    if (message === 'feature45') return msg.reply('Feature 45 triggered!');
-    if (message === 'feature46') return msg.reply('Feature 46 triggered!');
-    if (message === 'feature47') return msg.reply('Feature 47 triggered!');
-    if (message === 'feature48') return msg.reply('Feature 48 triggered!');
-    if (message === 'feature49') return msg.reply('Feature 49 triggered!');
-    if (message === 'feature50') return msg.reply('Feature 50 triggered!');
-    if (message === 'feature51') return msg.reply('Feature 51 triggered!');
-    if (message === 'feature52') return msg.reply('Feature 52 triggered!');
-    if (message === 'feature53') return msg.reply('Feature 53 triggered!');
-    if (message === 'feature54') return msg.reply('Feature 54 triggered!');
-    if (message === 'feature55') return msg.reply('Feature 55 triggered!');
-    if (message === 'feature56') return msg.reply('Feature 56 triggered!');
-    if (message === 'feature57') return msg.reply('Feature 57 triggered!');
-    if (message === 'feature58') return msg.reply('Feature 58 triggered!');
-    if (message === 'feature59') return msg.reply('Feature 59 triggered!');
-    if (message === 'feature60') return msg.reply('Feature 60 triggered!');
-    if (message === 'feature61') return msg.reply('Feature 61 triggered!');
-    if (message === 'feature62') return msg.reply('Feature 62 triggered!');
-    if (message === 'feature63') return msg.reply('Feature 63 triggered!');
-    if (message === 'feature64') return msg.reply('Feature 64 triggered!');
-    if (message === 'feature65') return msg.reply('Feature 65 triggered!');
-    if (message === 'feature66') return msg.reply('Feature 66 triggered!');
-    if (message === 'feature67') return msg.reply('Feature 67 triggered!');
-    if (message === 'feature68') return msg.reply('Feature 68 triggered!');
-    if (message === 'feature69') return msg.reply('Feature 69 triggered!');
-    if (message === 'feature70') return msg.reply('Feature 70 triggered!');
-    if (message === 'feature71') return msg.reply('Feature 71 triggered!');
-    if (message === 'feature72') return msg.reply('Feature 72 triggered!');
-    if (message === 'feature73') return msg.reply('Feature 73 triggered!');
-    if (message === 'feature74') return msg.reply('Feature 74 triggered!');
-    if (message === 'feature75') return msg.reply('Feature 75 triggered!');
-    if (message === 'feature76') return msg.reply('Feature 76 triggered!');
-    if (message === 'feature77') return msg.reply('Feature 77 triggered!');
-    if (message === 'feature78') return msg.reply('Feature 78 triggered!');
-    if (message === 'feature79') return msg.reply('Feature 79 triggered!');
-    if (message === 'feature80') return msg.reply('Feature 80 triggered!');
-    if (message === 'feature81') return msg.reply('Feature 81 triggered!');
-    if (message === 'feature82') return msg.reply('Feature 82 triggered!');
-    if (message === 'feature83') return msg.reply('Feature 83 triggered!');
-    if (message === 'feature84') return msg.reply('Feature 84 triggered!');
-    if (message === 'feature85') return msg.reply('Feature 85 triggered!');
-    if (message === 'feature86') return msg.reply('Feature 86 triggered!');
-    if (message === 'feature87') return msg.reply('Feature 87 triggered!');
-    if (message === 'feature88') return msg.reply('Feature 88 triggered!');
-    if (message === 'feature89') return msg.reply('Feature 89 triggered!');
-    if (message === 'feature90') return msg.reply('Feature 90 triggered!');
-    if (message === 'feature91') return msg.reply('Feature 91 triggered!');
-    if (message === 'feature92') return msg.reply('Feature 92 triggered!');
-    if (message === 'feature93') return msg.reply('Feature 93 triggered!');
-    if (message === 'feature94') return msg.reply('Feature 94 triggered!');
-    if (message === 'feature95') return msg.reply('Feature 95 triggered!');
-    if (message === 'feature96') return msg.reply('Feature 96 triggered!');
-    if (message === 'feature97') return msg.reply('Feature 97 triggered!');
-    if (message === 'feature98') return msg.reply('Feature 98 triggered!');
-    if (message === 'feature99') return msg.reply('Feature 99 triggered!');
-    if (message === 'feature100') return msg.reply('Feature 100 triggered!');
-    if (message === 'feature101') return msg.reply('Feature 101 triggered!');
-    if (message === 'feature102') return msg.reply('Feature 102 triggered!');
-    if (message === 'feature103') return msg.reply('Feature 103 triggered!');
-    if (message === 'feature104') return msg.reply('Feature 104 triggered!');
-    if (message === 'feature105') return msg.reply('Feature 105 triggered!');
-    if (message === 'feature106') return msg.reply('Feature 106 triggered!');
-    if (message === 'feature107') return msg.reply('Feature 107 triggered!');
-    if (message === 'feature108') return msg.reply('Feature 108 triggered!');
-    if (message === 'feature109') return msg.reply('Feature 109 triggered!');
-    if (message === 'feature110') return msg.reply('Feature 110 triggered!');
-    if (message === 'feature111') return msg.reply('Feature 111 triggered!');
-    if (message === 'feature112') return msg.reply('Feature 112 triggered!');
-    if (message === 'feature113') return msg.reply('Feature 113 triggered!');
-    if (message === 'feature114') return msg.reply('Feature 114 triggered!');
-    if (message === 'feature115') return msg.reply('Feature 115 triggered!');
-    if (message === 'feature116') return msg.reply('Feature 116 triggered!');
-    if (message === 'feature117') return msg.reply('Feature 117 triggered!');
-    if (message === 'feature118') return msg.reply('Feature 118 triggered!');
-    if (message === 'feature119') return msg.reply('Feature 119 triggered!');
-    if (message === 'feature120') return msg.reply('Feature 120 triggered!');
-    if (message === 'feature121') return msg.reply('Feature 121 triggered!');
-    if (message === 'feature122') return msg.reply('Feature 122 triggered!');
-    if (message === 'feature123') return msg.reply('Feature 123 triggered!');
-    if (message === 'feature124') return msg.reply('Feature 124 triggered!');
-    if (message === 'feature125') return msg.reply('Feature 125 triggered!');
-    if (message === 'feature126') return msg.reply('Feature 126 triggered!');
-    if (message === 'feature127') return msg.reply('Feature 127 triggered!');
-    if (message === 'feature128') return msg.reply('Feature 128 triggered!');
-    if (message === 'feature129') return msg.reply('Feature 129 triggered!');
-    if (message === 'feature130') return msg.reply('Feature 130 triggered!');
-    if (message === 'feature131') return msg.reply('Feature 131 triggered!');
-    if (message === 'feature132') return msg.reply('Feature 132 triggered!');
-    if (message === 'feature133') return msg.reply('Feature 133 triggered!');
-    if (message === 'feature134') return msg.reply('Feature 134 triggered!');
-    if (message === 'feature135') return msg.reply('Feature 135 triggered!');
-    if (message === 'feature136') return msg.reply('Feature 136 triggered!');
-    if (message === 'feature137') return msg.reply('Feature 137 triggered!');
-    if (message === 'feature138') return msg.reply('Feature 138 triggered!');
-    if (message === 'feature139') return msg.reply('Feature 139 triggered!');
-    if (message === 'feature140') return msg.reply('Feature 140 triggered!');
-    if (message === 'feature141') return msg.reply('Feature 141 triggered!');
-    if (message === 'feature142') return msg.reply('Feature 142 triggered!');
-    if (message === 'feature143') return msg.reply('Feature 143 triggered!');
-    if (message === 'feature144') return msg.reply('Feature 144 triggered!');
-    if (message === 'feature145') return msg.reply('Feature 145 triggered!');
-    if (message === 'feature146') return msg.reply('Feature 146 triggered!');
-    if (message === 'feature147') return msg.reply('Feature 147 triggered!');
-    if (message === 'feature148') return msg.reply('Feature 148 triggered!');
-    if (message === 'feature149') return msg.reply('Feature 149 triggered!');
-    if (message === 'feature150') return msg.reply('Feature 150 triggered!');
-    if (message === 'feature151') return msg.reply('Feature 151 triggered!');
-    if (message === 'feature152') return msg.reply('Feature 152 triggered!');
-    if (message === 'feature153') return msg.reply('Feature 153 triggered!');
-    if (message === 'feature154') return msg.reply('Feature 154 triggered!');
-    if (message === 'feature155') return msg.reply('Feature 155 triggered!');
-    if (message === 'feature156') return msg.reply('Feature 156 triggered!');
-    if (message === 'feature157') return msg.reply('Feature 157 triggered!');
-    if (message === 'feature158') return msg.reply('Feature 158 triggered!');
-    if (message === 'feature159') return msg.reply('Feature 159 triggered!');
-    if (message === 'feature160') return msg.reply('Feature 160 triggered!');
-    if (message === 'feature161') return msg.reply('Feature 161 triggered!');
-    if (message === 'feature162') return msg.reply('Feature 162 triggered!');
-    if (message === 'feature163') return msg.reply('Feature 163 triggered!');
-    if (message === 'feature164') return msg.reply('Feature 164 triggered!');
-    if (message === 'feature165') return msg.reply('Feature 165 triggered!');
-    if (message === 'feature166') return msg.reply('Feature 166 triggered!');
-    if (message === 'feature167') return msg.reply('Feature 167 triggered!');
-    if (message === 'feature168') return msg.reply('Feature 168 triggered!');
-    if (message === 'feature169') return msg.reply('Feature 169 triggered!');
-    if (message === 'feature170') return msg.reply('Feature 170 triggered!');
-    if (message === 'feature171') return msg.reply('Feature 171 triggered!');
-    if (message === 'feature172') return msg.reply('Feature 172 triggered!');
-    if (message === 'feature173') return msg.reply('Feature 173 triggered!');
-    if (message === 'feature174') return msg.reply('Feature 174 triggered!');
-    if (message === 'feature175') return msg.reply('Feature 175 triggered!');
-    if (message === 'feature176') return msg.reply('Feature 176 triggered!');
-    if (message === 'feature177') return msg.reply('Feature 177 triggered!');
-    if (message === 'feature178') return msg.reply('Feature 178 triggered!');
-    if (message === 'feature179') return msg.reply('Feature 179 triggered!');
-    if (message === 'feature180') return msg.reply('Feature 180 triggered!');
-    if (message === 'feature181') return msg.reply('Feature 181 triggered!');
-    if (message === 'feature182') return msg.reply('Feature 182 triggered!');
-    if (message === 'feature183') return msg.reply('Feature 183 triggered!');
-    if (message === 'feature184') return msg.reply('Feature 184 triggered!');
-    if (message === 'feature185') return msg.reply('Feature 185 triggered!');
-    if (message === 'feature186') return msg.reply('Feature 186 triggered!');
-    if (message === 'feature187') return msg.reply('Feature 187 triggered!');
-    if (message === 'feature188') return msg.reply('Feature 188 triggered!');
-    if (message === 'feature189') return msg.reply('Feature 189 triggered!');
-    if (message === 'feature190') return msg.reply('Feature 190 triggered!');
-    if (message === 'feature191') return msg.reply('Feature 191 triggered!');
-    if (message === 'feature192') return msg.reply('Feature 192 triggered!');
-    if (message === 'feature193') return msg.reply('Feature 193 triggered!');
-    if (message === 'feature194') return msg.reply('Feature 194 triggered!');
-    if (message === 'feature195') return msg.reply('Feature 195 triggered!');
-    if (message === 'feature196') return msg.reply('Feature 196 triggered!');
-    if (message === 'feature197') return msg.reply('Feature 197 triggered!');
-    if (message === 'feature198') return msg.reply('Feature 198 triggered!');
-    if (message === 'feature199') return msg.reply('Feature 199 triggered!');
-    if (message === 'feature200') return msg.reply('Feature 200 triggered!');
+    // AI ChatGPT
+    if (message.startsWith('ai ')) {
+        const prompt = message.slice(3);
+        try {
+            const response = await axios.post("https://api.openai.com/v1/completions", {
+                model: "text-davinci-003",
+                prompt: prompt,
+                max_tokens: 100,
+                temperature: 0.7
+            }, {
+                headers: {
+                    "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+                }
+            });
 
-    if (message.startsWith('feature')) return msg.reply('Hii feature ipo! ✅');
+            return msg.reply(response.data.choices[0].text.trim());
+        } catch (error) {
+            return msg.reply("AI response error.");
+        }
+    }
+
+    // Badword Filter
+    const badWords = ['fuck', 'shit', 'idiot', 'bitch'];
+    if (badWords.some(word => message.includes(word))) {
+        await msg.delete(true);
+        return msg.reply('⚠️ Please avoid using bad words!');
+    }
+
+    // Sticker Generator
+    if (msg.hasMedia && message === 'sticker') {
+        const media = await msg.downloadMedia();
+        msg.reply(media, null, { sendMediaAsSticker: true });
+    }
+
+    // Simulate 200+ Commands
+    for (let i = 1; i <= 200; i++) {
+        if (message === `feature${i}`) {
+            return msg.reply(`✅ Feature ${i} triggered!`);
+        }
+    }
+});
+
+client.on('group_join', async (notification) => {
+    const chat = await notification.getChat();
+    const contact = await notification.getContact();
+    chat.sendMessage(`Karibu sana ${contact.pushname || 'mpya member'} kwenye *${chat.name}*!`);
 });
 
 client.initialize();
